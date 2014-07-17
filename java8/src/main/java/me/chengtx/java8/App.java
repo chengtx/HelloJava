ee5d8df473530295e35722d9055e850dc0125a69
Tingxian <chengtingxian@gmail.com>
2014-07-17 11:12:18 +0800

    first commit

---
+++ java8/src/main/java/me/chengtx/java8/App.java
@@ -0,0 +1,13 @@
+package me.chengtx.java8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
