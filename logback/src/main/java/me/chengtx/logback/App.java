d8ab1ab09a8f03b00db5ff04505f387735723e75
Tingxian <chengtingxian@gmail.com>
2014-08-05 17:40:09 +0800

    add logback project
    Signed-off-by: Tingxian <chengtingxian@gmail.com>

---
+++ logback/src/main/java/me/chengtx/logback/App.java
@@ -0,0 +1,21 @@
+package me.chengtx.logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		logger.trace("logback: Hello world!");
+		logger.debug("logback: Hello world!");
+		logger.info("logback: Hello world!");
+		logger.warn("logback: Hello world!");
+	}
+}
