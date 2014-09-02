f11d4a9a628d2934fc9f7a42fe0051268cc20c49
Tingxian <chengtingxian@gmail.com>
2014-09-02 14:55:36 +0800

    Signed-off-by: Tingxian <chengtingxian@gmail.com>

--- java8/src/main/java/me/chengtx/java8/App.java
+++ java8/src/main/java/me/chengtx/java8/App.java
@@ -1,11 +1,26 @@
 package me.chengtx.java8;
 
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
 /**
  * Hello world!
  *
  */
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello Java!");
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder().include(App.class.getSimpleName())
+				.forks(1).build();
+
+		new Runner(opt).run();
+	}
+
+	@Benchmark
+	public static void wellHelloThere() {
+		// this method was intentionally left blank.
+		System.out.println("Hello There!");
 	}
 }
