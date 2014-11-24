c102c8c2c37ae5102b43c55ec908f7f3f3be89bf
Tingxian <chengtingxian@gmail.com>
2014-11-24 15:41:27 +0800

    add idea support
    Signed-off-by: Tingxian <chengtingxian@gmail.com>

---
+++ webapp/src/test/java/me/chengtx/webapp/rest/MyResourceTest.java
@@ -0,0 +1,23 @@
+package me.chengtx.webapp.rest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MyResourceTest {
+
+    @Test
+    public void testGetAllUser() throws Exception {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testGetSysProp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetString() throws Exception {
+
+    }
+}
