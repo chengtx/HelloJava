13b7cd6f78a0ff9e24722b07ac5be6e84fcf7730
Tingxian <chengtingxian@gmail.com>
2014-11-25 15:07:43 +0800

    modify maven setting for idea

--- java8/src/main/java/me/chengtx/java8/contest/SortTest.java
+++ java8/src/main/java/me/chengtx/java8/contest/SortTest.java
@@ -1,5 +1,7 @@
 package me.chengtx.java8.contest;
 
+import org.junit.Test;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -7,49 +9,45 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Random;
 
-import org.junit.Test;
-
 /**
  * @author chengt4
- *
  */
 public class SortTest {
 
-	private static Path input_path = Paths.get("input.txt");
+    private static Path input_path = Paths.get("input.txt");
 
-	private static final int NUMBER = 500000;
-	// private static final long UPPER = 10000000000L;
-	private static final Random rnd = new Random();
+    private static final int NUMBER = 500000;
+    // private static final long UPPER = 10000000000L;
+    private static final Random rnd = new Random();
 
-	// @Test
-	public void initInput() {
+    @Test
+    public void initInput() {
 
-		int count = 0;
+        int count = 0;
 
-		try (BufferedWriter bw = Files.newBufferedWriter(input_path);) {
-			bw.write(String.valueOf(NUMBER));
-			bw.newLine();
-			for (;;) {
-				int raw = rnd.nextInt();
-				if (raw >= 0 && raw <= 10000000000L) {
-					count++;
-					bw.write(String.valueOf(raw));
-					bw.write(" ");
-					if (count >= NUMBER) {
-						break;
-					}
-				}
-			}
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			return;
-		}
-	}
+        try (BufferedWriter bw = Files.newBufferedWriter(input_path)) {
+            bw.write(String.valueOf(NUMBER));
+            bw.newLine();
+            for (; ; ) {
+                int raw = rnd.nextInt();
+                if (raw >= 0 && raw <= 10000000000L) {
+                    count++;
+                    bw.write(String.valueOf(raw));
+                    bw.write(" ");
+                    if (count >= NUMBER) {
+                        break;
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	@Test
-	public void test() {
-		Sort.start();
-		
-	}
+    @Test
+    public void test() {
+        Sort.start();
+
+    }
 
 }
