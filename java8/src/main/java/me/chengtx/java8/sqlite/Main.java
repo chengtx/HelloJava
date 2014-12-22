a2894431ec53178b015ab6ae9da4e2141a954399
Tingxian <chengtingxian@gmail.com>
2014-12-22 10:13:42 +0800

    update java 8

---
+++ java8/src/main/java/me/chengtx/java8/sqlite/Main.java
@@ -0,0 +1,11 @@
+package me.chengtx.java8.sqlite;
+
+/**
+ * @author <a href="mailto:chengtingxian@gmail.com">Tingxian Cheng</a>
+ * @version 12/22/2014
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Sqlite!");
+    }
+}
